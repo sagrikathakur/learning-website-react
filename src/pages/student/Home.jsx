cd43f2b5e1fa301c55d4374d3a471fd6b6d8e8d4
sagrika thakur <sagrikathakur68@gmail.com>
2026-05-11 08:36:39 +0530

    Fix case sensitivity in imports

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Hero from '../../Components/student/Hero'
-import Companies from '../../Components/student/Companies'
-import CouesesSection from '../../Components/student/CouesesSection'
-import TestimonialsSection from '../../Components/student/TestimonialsSection'
-import CallToAction from '../../Components/student/CallToAction'
-import Footer from '../../Components/student/Footer'
+import Hero from '../../components/student/Hero'
+import Companies from '../../components/student/Companies'
+import CouesesSection from '../../components/student/CouesesSection'
+import TestimonialsSection from '../../components/student/TestimonialsSection'
+import CallToAction from '../../components/student/CallToAction'
+import Footer from '../../components/student/Footer'
 
 
 
